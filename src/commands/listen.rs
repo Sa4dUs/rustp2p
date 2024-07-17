cc95dbda33cf9fc6dcb525faff90b51f612df607
Marcelo Domínguez <69964857+Sa4dUs@users.noreply.github.com>
2024-07-17 09:44:31 +0200

    feat: bigger files support

--- src/commands/listen.rs
+++ src/commands/listen.rs
@@ -1,8 +1,9 @@
+use chrono::Utc;
 use clap::Error;
+use std::net::SocketAddr;
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
 use tokio::{io::AsyncReadExt, net::TcpListener};
-
-use crate::utils::files::write_from_buffer;
-
 
 pub async fn listen() -> Result<(), Error> {
     let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
@@ -10,24 +11,49 @@ pub async fn listen() -> Result<(), Error> {
 
     loop {
         let (mut socket, addr) = listener.accept().await.unwrap();
-        println!("[rustp2p::commands::listen.rs::listen] (socket, addr) = ({:#?}, {})", socket, addr);
+        println!(
+            "[rustp2p::commands::listen.rs::listen] (socket, addr) = ({:#?}, {})",
+            socket, addr
+        );
 
         tokio::spawn(async move {
             let mut buffer = vec![0; 1024];
+            let mut file_buffer = Vec::new();
 
-            match socket.read(&mut buffer).await {
-                Ok(0) => {
-                    println!("[rustp2p::commands::listen.rs::listen] Connection closed");
+            loop {
+                match socket.read(&mut buffer).await {
+                    Ok(0) => {
+                        println!("[rustp2p::commands::listen.rs::listen] Connection closed");
+                        break;
+                    }
+                    Ok(n) => {
+                        println!(
+                            "[rustp2p::commands::listen.rs::listen] Received {} bytes",
+                            n
+                        );
+                        file_buffer.extend_from_slice(&buffer[..n]);
+                    }
+                    Err(e) => {
+                        println!("[rustp2p::commands::listen.rs::listen] Failed to read from socket; err = {:?}", e);
+                        return;
+                    }
                 }
-                Ok(n) => {
-                    println!("[rustp2p::commands::listen.rs::listen] Received {} bytes: {:?}", n, &buffer[..n]);
+            }
 
-                    write_from_buffer(addr, &buffer[..n]).await.unwrap_or_else(|_| eprintln!("[rustp2p::commands::listen.rs::listen] Couldn't write to file"));
-                }
-                Err(e) => {
-                    println!("[rustp2p::commands::listen.rs::listen] Failed to read from socket; err = {:?}", e);
-                }
+            if !file_buffer.is_empty() {
+                write_to_single_file(addr, &file_buffer)
+                    .await
+                    .unwrap_or_else(|_| {
+                        eprintln!("[rustp2p::commands::listen.rs::listen] Couldn't write to file")
+                    });
             }
         });
     }
+}
+
+async fn write_to_single_file(addr: SocketAddr, buffer: &[u8]) -> Result<(), Error> {
+    let filename = format!("{}-{}.dat", addr.ip(), Utc::now().timestamp());
+    let mut f = File::create(filename).await?;
+    f.write_all(buffer).await?;
+    Ok(())
 }
--- src/commands/send.rs
+++ src/commands/send.rs
@@ -1,17 +1,25 @@
 use clap::Error;
 use tokio::{io::AsyncWriteExt, net::TcpStream};
 use crate::utils::files::read_to_buffer;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
 
 pub async fn send(address: String, file: String) -> Result<(), Error> {
-    let mut buffer:[u8; 1024] = [0; 1024];
-    let n = read_to_buffer(&file, &mut buffer).await?;
-
     println!("[rustp2p::commands::send.rs::send] {} {}", address, file);
     
     let mut stream = TcpStream::connect(&address).await?;
     println!("[rustp2p::commands::send.rs::send] Connected to the server at {}", address);
 
-    stream.write_all(&buffer[..n]).await?;
+    let mut file = File::open(file).await?;
+    let mut buffer = [0u8; 1024];
+
+    loop {
+        let n = file.read(&mut buffer).await?;
+        if n == 0 {
+            break;
+        }
+        stream.write_all(&buffer[..n]).await?;
+    }
 
     Ok(())
 }
